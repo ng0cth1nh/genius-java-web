b804b3eb1c6c5b8bc2567e2047634fb300dab389
Vu Ngoc Thinh <64978350+ng0cth1nh@users.noreply.github.com>
2021-02-14 19:12:54 +0700

    update edit.js

--- web/script/edit.js
+++ web/script/edit.js
@@ -3,65 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+const form = document.getElementById('form');
+const date = document.getElementById('date');
+const year = document.getElementById('year');
+const month = document.getElementById('month');
+const pass = document.getElementById('password');
+const rePass = document.getElementById('rePassword');
+const eRePass = document.getElementById('error-repassword');
+const eDate = document.getElementById('error-date');
 
 
+form.addEventListener('submit', (e) => {
+  if (pass.value.localeCompare(rePass.value) != 0) {
+    e.preventDefault();
+    eRePass.style.display = 'flex';
+  } else {
+    eRePass.style.display = 'none';
+  }
+  if (isValidDate(month.value + "/" + date.value + "/" + year.value) == false) {
+    e.preventDefault();
+    eDate.style.display = 'flex';
+  } else {
+    eDate.style.display = 'none';
+  }
+})
 
-            const form = document.getElementById('form');
-        
-            const date = document.getElementById('date');
-            const year = document.getElementById('year');
-            const month = document.getElementById('month');
-            
-            const pass = document.getElementById('password');
-            const rePass = document.getElementById('rePassword');
-            const eRePass = document.getElementById('error-repassword');
-            const eDate = document.getElementById('error-date');
+function isValidDate(dateString) {
+  // First check for the pattern
+  if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+    return false;
+  // Parse the date parts to integers
+  let parts = dateString.split("/");
+  let day = parseInt(parts[1], 10);
+  let month = parseInt(parts[0], 10);
+  let year = parseInt(parts[2], 10);
+  // Check the ranges of month and year
+  if (year < 1000 || year > 3000 || month == 0 || month > 12)
+    return false;
+  let monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  // Adjust for leap years
+  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+    monthLength[1] = 29;
 
-
-            form.addEventListener('submit', (e) => {
-                if (pass.value.localeCompare(rePass.value) != 0) {
-                    e.preventDefault();
-                    eRePass.style.display = 'flex';
-                }else{
-                    eRePass.style.display = 'none';
-                }
-                if (isValidDate(month.value + "/" + date.value + "/" + year.value) == false) {
-                    e.preventDefault();
-                    eDate.style.display = 'flex';
-                }else{
-                    eDate.style.display = 'none';
-                }
-
-
-
-            });
-
-
-            function isValidDate(dateString)
-            {
-                // First check for the pattern
-                if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
-                    return false;
-
-                // Parse the date parts to integers
-                var parts = dateString.split("/");
-                var day = parseInt(parts[1], 10);
-                var month = parseInt(parts[0], 10);
-                var year = parseInt(parts[2], 10);
-
-                // Check the ranges of month and year
-                if (year < 1000 || year > 3000 || month == 0 || month > 12)
-                    return false;
-
-                var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-                // Adjust for leap years
-                if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-                    monthLength[1] = 29;
-
-                // Check the range of the day
-                return day > 0 && day <= monthLength[month - 1];
-            }
-            ;
-
+  // Check the range of the day
+  return day > 0 && day <= monthLength[month - 1];
+}
 
